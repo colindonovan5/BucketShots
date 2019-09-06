bc79d215ccfb430549914bfb7ce93bcd03d0e8c3
colindonovan5 <32983190+colindonovan5@users.noreply.github.com>
2019-09-06 01:12:21 -0400

    add shot-preview component

---
+++ BucketShots/src/app/User/User.ts
@@ -0,0 +1,12 @@
+import { Shot } from '../Shot';
+
+export class User {
+    userName: string;
+    shotList: Shot[];
+    /**
+     * The class for a registered user of the site
+     */
+    constructor() {
+
+    }
+}
---
+++ BucketShots/src/app/shot-detail/shot-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shot-detail',
+  templateUrl: './shot-detail.component.html',
+  styleUrls: ['./shot-detail.component.css']
+})
+export class ShotDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
